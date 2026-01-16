e97463302682a28b97110ed8e5d53753c2b2d50b
Nirvexor <eneskarakaya5418@gmail.com>
2026-01-16 23:25:52 +0300

    An algorithm to detect the king of the hill

---
+++ King_of_the_hill.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1. Create the Array (fixed size of 5)
+    int score[5] = {10, 25, 50, 75, 100};
+    int index_of_the_highest_score = 0;
+    int maxScore = score[0];
+
+    // 2. Loop through the Array
+    // Notice: We go from 0 to 4 (i < 5).
+    // if you try scores[5], you crash (Index Out of Bounds).
+
+    cout << "Reading Scores" << endl;
+    
+    for (int i = 1; i < 5; i++){
+        if (score[i] > maxScore) {
+            maxScore = score[i];
+            index_of_the_highest_score = i;
+        }
+    }
+    cout << "The highest score is "<< maxScore << " at index" << index_of_the_highest_score;
+
+    return 0;
+}
